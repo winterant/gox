5460080df7a874e5d79ff2bf911a18cb101d9400
Jinglong <winterant@163.com>
2025-03-11 20:27:05 +0800

    fix xlog invalid option

--- pkg/xlog/default_logger.go
+++ pkg/xlog/default_logger.go
@@ -15,19 +15,21 @@ var defaultLogger *Logger
 func InitDefault(opt Option) {
 	_ = defaults.Set(&opt)
 
-	var logWriter io.Writer = &lumberjack.Logger{
-		Filename:   opt.Path,       // defaultLog file path
-		MaxSize:    opt.MaxSizeMB,  // file max size in MB
-		MaxBackups: opt.MaxBackups, // max number of backup defaultLog files
-		MaxAge:     opt.MaxDays,    // max number of days to keep old files
-		Compress:   false,          // whether to compress/archive old files
-		LocalTime:  true,           // Use local time or not
-	}
-	if opt.Stdout {
-		logWriter = io.MultiWriter(logWriter, os.Stdout)
+	if opt.Writer == nil {
+		opt.Writer = &lumberjack.Logger{
+			Filename:   opt.Path,       // defaultLog file path
+			MaxSize:    opt.MaxSizeMB,  // file max size in MB
+			MaxBackups: opt.MaxBackups, // max number of backup defaultLog files
+			MaxAge:     opt.MaxDays,    // max number of days to keep old files
+			Compress:   false,          // whether to compress/archive old files
+			LocalTime:  true,           // Use local time or not
+		}
+		if opt.Stdout {
+			opt.Writer = io.MultiWriter(opt.Writer, os.Stdout)
+		}
 	}
 	sLevel := getSlogLevel(opt.Level)
-	handler := newPrettyHandler(withWriter(logWriter), withLever(sLevel), withCallerDepth(3), withCodeSource(true))
+	handler := newPrettyHandler(withWriter(opt.Writer), withLever(sLevel), withCallerDepth(3), withCodeSource(true))
 	defaultLogger = &Logger{
 		Logger: slog.New(handler),
 	}
