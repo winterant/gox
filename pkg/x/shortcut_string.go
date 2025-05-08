54ec64b5283947bcafef8248302220f3fe476809
Jinglong <winterant@163.com>
2025-05-08 20:42:27 +0800

    add IsBlank(string)

--- pkg/x/shortcut_string.go
+++ pkg/x/shortcut_string.go
@@ -5,6 +5,15 @@ import (
 	"encoding/hex"
 )
 
+func IsBlank(s string) bool {
+	for i := range s {
+		if s[i] != ' ' && s[i] != '\t' && s[i] != '\n' && s[i] != '\r' {
+			return false
+		}
+	}
+	return true
+}
+
 // Md5 returns the MD5 string which is 32 bytes long only containing hexadecimal characters.
 func Md5(data string) string {
 	hasher := md5.New()
