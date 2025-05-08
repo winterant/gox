7f0a835c77f2c1ac315ca538e1abf713ce7f01d3
Jinglong <winterant@163.com>
2025-05-08 15:13:23 +0800

    add func In to check whether obj in List

--- pkg/x/shortcut_logic.go
+++ pkg/x/shortcut_logic.go
@@ -12,3 +12,12 @@ func If[T any](condition bool, trueVal T, falseVal T) T {
 	}
 	return falseVal
 }
+
+func In[T comparable](val T, list []T) bool {
+	for _, v := range list {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
