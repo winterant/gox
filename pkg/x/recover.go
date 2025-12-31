23ca54c276338efcf9f9ffa8e67e6b7e524dc3c2
Jinglong <winterant@163.com>
2025-12-31 21:11:10 +0800

    improve x.Recover error

--- pkg/x/recover.go
+++ pkg/x/recover.go
@@ -2,22 +2,56 @@ package x
 
 import (
 	"fmt"
+	"io"
 
 	"github.com/winterant/gox/pkg/xcaller"
 )
 
-// Recover Deprecated catch panic and recover. call it at start of goroutine.
+type fundamental struct {
+	message string
+	stack   *xcaller.CallerStack
+}
+
+func (e *fundamental) Error() string { return e.message }
+
+func (e *fundamental) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 'v':
+		if s.Flag('+') {
+			fmt.Fprintf(s, "%s\n%s", e.message, e.stack.String())
+			return
+		}
+		fallthrough
+	case 's':
+		io.WriteString(s, e.message)
+	case 'q':
+		fmt.Fprintf(s, "%q", e.message)
+	}
+}
+
+// Recover catch panic and recover. call it at start of goroutine.
 // For example:
 //
 //	go func(){
-//		defer Recover(func(err error){
+//		defer Recover(func(err error){  // must start with defer !!!!!!
 //			log.Println(err)
 //		})
 //		...
 //	}()
+//
+// Wrong example:
+//
+//	defer func(){
+//		Recover(func(err error){  // must start with defer !!!!!! but not! it will not catch panic!!!!!!
+//			log.Println(err)
+//		})
+//	}()
 func Recover(f func(error)) {
 	if r := recover(); r != nil {
-		err := fmt.Errorf("%+v\n%s", r, xcaller.GetCallerStack(2).String())
+		err := &fundamental{
+			message: fmt.Sprintf("%+v", r),
+			stack:   xcaller.GetCallerStack(2),
+		}
 		f(err)
 	}
 }
